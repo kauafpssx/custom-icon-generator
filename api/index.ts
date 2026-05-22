7fc4831b335a2bc18bd98795aaf1d490aa02ab31
zKauaFerreira <kaua@agencyos.com>
2026-05-22 19:27:08 -0300

    fix: add .js extension to handler import for ESM compatibility
    - add .js extension to import of ./_lib/handler for ESM/CJS resolution
    
    Co-Authored-By: GithubAgent (Powered by @kauafpss_)

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import { IncomingMessage, ServerResponse } from 'http';
-import { handleApiRequest } from './_lib/handler';
+import { handleApiRequest } from './_lib/handler.js';
 
 export default async function handler(req: IncomingMessage, res: ServerResponse) {
   return handleApiRequest(req, res);
